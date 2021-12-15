423a704088ce54040d2250d1421270f4d4d2c25a
Shubham Patel <ptlshubham@gmail.com>
2021-12-15 10:56:18 +0530

    Error resolved in chatbox

--- src/app/chatbox/chatbox.component.ts
+++ src/app/chatbox/chatbox.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import * as $ from 'jquery';
 @Component({
   selector: 'app-chatbox',
   templateUrl: './chatbox.component.html',
@@ -10,9 +9,9 @@ export class ChatboxComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    $('#action_menu_btn').click(function () {
-      $('.action_menu').toggle();
-    });
+    // $('#action_menu_btn').click(function () {
+    //   $('.action_menu').toggle();
+    // });
   }
 
 }
